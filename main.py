1be87300464ccdd53abd1ddc47c7a1d0a70c3736
Kenny Santanu <31720797+kennysantanu@users.noreply.github.com>
2024-10-03 22:53:32 -0700

    feat: Read previous messages in a channel

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ token = os.getenv('DISCORD_TOKEN')
 ollama_url = os.getenv('OLLAMA_URL')
 ollama_model = os.getenv('OLLAMA_MODEL')
 
+if os.getenv('HISTORY_LIMIT') == '':
+    history_limit = 32
+else:
+    history_limit = int(os.getenv('HISTORY_LIMIT'))
+
 ollama = Client(host=ollama_url)
 
 intents = discord.Intents.default()
@@ -22,16 +27,30 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    print("Message: ", message.content)
+    print(f'NEW REQUEST FROM: {message.author} IN {message.channel}')
 
-    response = ollama.chat(model=ollama_model, messages=[
-        {
-            'role': 'user',
-            'content': message.content,
-        },
-    ])
+    history = []
+    channel = message.channel
+    
+    # Retrieve the message history of the current channel (limit the number of messages)
+    async for msg in channel.history(limit=history_limit):
 
-    print("Response: ", response['message']['content'])
+        if msg.author == client.user:
+            history.insert(0, {
+                'role': 'assistant',
+                'content': msg.content,
+            })
+        else:
+            history.insert(0, {
+                'role': 'user',
+                'content': msg.author.display_name + ': ' + msg.content
+            })
+    
+    print("HISTORY: ", history)
+
+    response = ollama.chat(model=ollama_model, messages=history)
+    print("RESPONSE: ", response['message']['content'])
+    
     await message.channel.send(response['message']['content'])
 
 client.run(token)
