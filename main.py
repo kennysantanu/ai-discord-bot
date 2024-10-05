551c462e9ae4438ca2644fad971c904c31027d1a
Kenny Santanu <31720797+kennysantanu@users.noreply.github.com>
2024-10-04 23:59:37 -0700

    feat: Add response time limit before wake words or @mention is needed

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 import discord
 from ollama import Client
+from datetime import timedelta
 
 token = os.getenv('DISCORD_TOKEN')
 ollama_url = os.getenv('OLLAMA_URL')
 ollama_model = os.getenv('OLLAMA_MODEL')
 
-history_limit = 32 if os.getenv('HISTORY_LIMIT') == '' else int(os.getenv('HISTORY_LIMIT'))
 wake_words = [word.strip() for word in os.getenv('WAKE_WORDS').split(',')] if os.getenv('WAKE_WORDS') else []
+history_limit = 32 if os.getenv('HISTORY_LIMIT') == '' else int(os.getenv('HISTORY_LIMIT'))
+response_time = 60 if os.getenv('RESPONSE_TIME') == '' else int(os.getenv('RESPONSE_TIME'))
 
 ollama = Client(host=ollama_url)
 
@@ -16,24 +18,31 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+last_response = {}
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
     print(f'Ollama URL: {ollama_url}')
     print(f'Model: {ollama_model}')
-    print(f'History Limit: {history_limit}')
     print(f'Wake Words: {wake_words}')
+    print(f'History Limit: {history_limit}')
+    print(f'Response Time: {response_time}')
 
 @client.event
 async def on_message(message):
     # Ignore messages from the bot itself
     if message.author == client.user:
         return
-    
-    # Ignore messages that do not contain bot mention or wake words
-    if client.user.mentioned_in(message) is False and any(word in message.content for word in wake_words) is False:
-        return
 
+    # Ignore messages if the bot is not mentioned, the message does not contain the wake words, and the last response was not recent
+    mentioned = client.user.mentioned_in(message)
+    contains_wake_word = any(word in message.content for word in wake_words)
+    recent_response = (discord.utils.utcnow() < last_response.get(message.channel.name) + timedelta(seconds=response_time)) if message.channel.name in last_response else False
+
+    if not mentioned and not contains_wake_word and not recent_response:
+        return
+    
     # Accept new request
     print(f'NEW REQUEST FROM: [{message.author}] IN [{message.channel}]')
 
@@ -63,5 +72,6 @@ async def on_message(message):
 
         # Send the response back to the channel
         await message.channel.send(response['message']['content'])
+        last_response[message.channel.name] = discord.utils.utcnow()
 
 client.run(token)
