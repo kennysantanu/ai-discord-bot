e52c2faa5618993d8d8038da98f21eb89fe7484d
Kenny Santanu <31720797+kennysantanu@users.noreply.github.com>
2024-10-08 01:04:46 -0700

    feat: Add /draw command for stable diffusion image generation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
+import json
+import requests
+import base64
+import tempfile
 import discord
+from discord import app_commands
 from discord.ext import commands
 from ollama import Client
 from datetime import timedelta
@@ -7,6 +12,10 @@ from datetime import timedelta
 token = os.getenv('DISCORD_TOKEN')
 ollama_url = os.getenv('OLLAMA_URL')
 ollama_model = os.getenv('OLLAMA_MODEL')
+stable_diffusion_url = os.getenv('STABLE_DIFFUSION_URL')
+extra_positive_prompt = os.getenv('POSITIVE_PROMPT')
+extra_negative_prompt = os.getenv('NEGATIVE_PROMPT')
+styles = [style.strip() for style in os.getenv('STYLES').split(',')] if os.getenv('STYLES') else []
 
 wake_words = [word.strip() for word in os.getenv('WAKE_WORDS').split(',')] if os.getenv('WAKE_WORDS') else []
 history_limit = 32 if os.getenv('HISTORY_LIMIT') == '' else int(os.getenv('HISTORY_LIMIT'))
@@ -38,6 +47,40 @@ async def quiet(interaction: discord.Interaction):
     last_response.pop(interaction.channel.name, None)
     await interaction.response.send_message("I'll be quiet now.")
 
+# Image generation command
+@bot.tree.command(name="draw", description="Generate an image based on the given text.")
+@app_commands.describe(
+    prompt="Describe the image you want to generate."
+)
+async def draw(interaction: discord.Interaction, prompt: str):
+
+    print(f'NEW DRAW REQUEST FROM: [{interaction.user.name}] IN [{interaction.channel}]')
+
+    # Let the user know that the request is being processed
+    await interaction.response.defer()    
+
+    # Load JSON file
+    with open('stable-diffusion.json') as f:
+        payload = json.load(f)
+        payload["prompt"] = prompt + ", " + extra_positive_prompt
+        payload["negative_prompt"] = extra_negative_prompt
+        payload["styles"] = styles
+
+    print("PROMPT:", prompt)
+
+    # Send the request to the Stable Diffusion API
+    response = requests.post(url=f'{stable_diffusion_url}/sdapi/v1/txt2img', json=payload)
+    r = response.json()
+    print("RESPONSE:", r["info"])
+
+    # Decode and save the image to a temporary file.
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
+        temp_file.write(base64.b64decode(r['images'][0]))
+        temp_file_path = temp_file.name
+
+    # Send the image back to the channel.
+    await interaction.followup.send("Here is my drawing of " + prompt, files=[discord.File(temp_file_path)])
+
 @bot.event
 async def on_message(message):
     # Ignore messages from the bot itself
@@ -53,7 +96,7 @@ async def on_message(message):
         return
     
     # Accept new request
-    print(f'NEW REQUEST FROM: [{message.author}] IN [{message.channel}]')
+    print(f'NEW CHAT REQUEST FROM: [{message.author}] IN [{message.channel}]')
 
     history = []
     channel = message.channel
